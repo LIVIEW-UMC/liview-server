b002842f27caf03d8c41399c23606319795c85c9
이진 <ctce7226@gmail.com>
2024-01-16 18:13:08 +0900

    feat: first commit

---
+++ src/main/java/umc/liview/LiviewApplication.java
@@ -0,0 +1,13 @@
+package umc.liview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/umc/liview/LiviewApplicationTests.java
@@ -0,0 +1,13 @@
+package umc.liview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
